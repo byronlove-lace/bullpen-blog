a4a0585a9719f2d3bc19e6d2560f7e9d5b3d9921
byronlove-lace <byronlovelace@proton.me>
2025-05-29 18:11:13 +0700

    Added confirmation token functionality

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
+from flask import current_app
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db, login_manager
+from itsdangerous import URLSafeTimedSerializer as Serializer
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -25,6 +27,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.Text)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    confirmed = db.Column(db.Boolean, default=False)
 
     def __repr__(self):
         return '<User {!r}>'.format(self.username)
@@ -39,6 +42,23 @@ class User(UserMixin, db.Model):
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+    
+    def gernerate_confirmation_token(self):
+            s = Serializer(current_app.config['SECRET_KEY'], salt='activate')
+            return s.dumps({'confirm': self.id})
+
+    def confirm(self, token, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token, max_age=expiration)
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
+
 
     def __init__(self, username=None, password=None, email=None):
         if username:
