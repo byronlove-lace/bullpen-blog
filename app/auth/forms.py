7fd4f1e32bd324aa836d078373e0163dfc8df3d6
byronlove-lace <byronlovelace@proton.me>
2025-05-29 18:09:12 +0700

    Added username and password validators to Registration form.

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -15,11 +15,11 @@ class LoginForm(FlaskForm):
 
 class RegistrationForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
-    username = StringField('Username', validators=[DataRequired, Length(1, 64), 
+    username = StringField('Username', validators=[DataRequired(), Length(1, 64), 
                                                    Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
                                                           'Usernames must have only letters,'
                                                           'numbers, dots or underscores')])
-    password = PasswordField('Password', validators=[DataRequired, Length(8, 128, message='Password must be between 8 and 128 characters long.'), 
+    password = PasswordField('Password', validators=[DataRequired(), Length(8, 128, message='Password must be between 8 and 128 characters long.'), 
                                                      EqualTo('password2', message='Passwords must match.')])
     password2 = PasswordField('Confirm password', validators=[DataRequired()])
     submit = SubmitField('Log In')
