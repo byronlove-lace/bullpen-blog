35a3cbf552b6ae040f25ba2997f9df98c0afdbf8
byronlove-lace <byronlovelace@proton.me>
2025-08-26 16:57:42 +0100

    Added before_app_request redirect for unconfirmed users.

--- app/auth/views.py
+++ app/auth/views.py
@@ -54,3 +54,11 @@ def confirm(token):
     else:
         flash('The confirmation link is invalid or has expired.')
     return redirect(url_for('main.index'))
+
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated \
+            and not current_user.confirmed \
+            and request.blueprint != 'auth' \
+            and request.endpoint != 'static':
+        return redirect(url_for('auth.unconfirmed'))
