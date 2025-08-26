c4d98d7f8a7163c097b39274485f11a53a2b1000
byronlove-lace <byronlovelace@proton.me>
2025-08-26 16:10:23 +0100

    Typo.

--- app/email.py
+++ app/email.py
@@ -3,7 +3,6 @@ from flask import render_template, current_app
 from flask_mail import Message
 from . import mail, logger
 
-
 def send_async_email(app, msg):
     with app.app_context():
         try:
@@ -14,7 +13,7 @@ def send_async_email(app, msg):
 
 def send_email(to, subject, template, **kwargs):
     msg = Message(current_app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
-                  sender=current_app.config['FLASK_MAIL_SENDER'], recipients=[to])
+                  sender=current_app.config['FLASKY_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
     thr = Thread(target=send_async_email, args=[current_app, msg])
