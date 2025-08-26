0953b9c67169e521a0cc57d8b5e4658f457f313a
byronlove-lace <byronlovelace@proton.me>
2025-08-26 16:06:30 +0100

    Added Login form

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,8 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms import BooleanField, PasswordField, StringField, SubmitField
+from wtforms.validators import DataRequired, Length, Email
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     secret = PasswordField('Tell me your secret.', validators=[DataRequired(), Length(6)])
     submit = SubmitField('Submit')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64),
+                                             Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Keep me logged in')
+    submit = SubmitField('Log In')
