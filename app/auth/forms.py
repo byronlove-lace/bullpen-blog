a92adcfa7783970a266340525ff7ef275bf429a1
byronlove-lace <byronlovelace@proton.me>
2025-08-27 16:20:51 +0100

    Added Change Password Form.

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -22,7 +22,7 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired(), Length(8, 128, message='Password must be between 8 and 128 characters long.'),
                                                      EqualTo('password2', message='Passwords must match.')])
     password2 = PasswordField('Confirm password', validators=[DataRequired()])
-    submit = SubmitField('Log In')
+    submit = SubmitField('Register')
 
     def __get_pwned(self, password):
         sha1 = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
@@ -46,3 +46,26 @@ class RegistrationForm(FlaskForm):
         threshold = 100
         if count and count > threshold:
             raise ValidationError(f'This password has appeared in {count} data breaches. Please choose a more secure ont.')
+
+class ChangePasswordForm(FlaskForm):
+    old_password = PasswordField('Old password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(8, 128, message='Password must be between 8 and 128 characters long.'),
+                                                     EqualTo('password2', message='Passwords must match.')])
+    password2 = PasswordField('Confirm password', validators=[DataRequired()])
+
+    def __get_pwned(self, password):
+        sha1 = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+        prefix, suffix = sha1[:5], sha1[5:]
+        response = requests.get(f'https://api.pwnedpasswords.com/range/{prefix}')
+        hashes = (line.split(':') for line in response.text.splitlines())
+        for h, count in hashes:
+            if h == suffix:
+                return int(count)
+
+    def validate_password(self, field):
+        count = self.__get_pwned(field.data)
+        threshold = 100
+        if count and count > threshold:
+            raise ValidationError(f'This password has appeared in {count} data breaches. Please choose a more secure ont.')
+
+    submit = SubmitField('Update Password')
