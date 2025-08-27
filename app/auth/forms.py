1a8795f07c7d3cfe0dad674cef9018f5a8c76f76
byronlove-lace <byronlovelace@proton.me>
2025-08-27 18:20:34 +0100

    Added Password Reset Form and Password Reset Request Form (Forgot your password)?

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -69,3 +69,40 @@ class ChangePasswordForm(FlaskForm):
             raise ValidationError(f'This password has appeared in {count} data breaches. Please choose a more secure ont.')
 
     submit = SubmitField('Update Password')
+
+class PasswordResetForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
+    submit = SubmitField('Reset Password')
+
+    def validate_email(self, field):
+        if not User.query.filter_by(email=field.data).first():
+            raise ValidationError('No account found with this email.')
+
+    password = PasswordField('Password', validators=[DataRequired(), Length(8, 128, message='Password must be between 8 and 128 characters long.'),
+                                                     EqualTo('password2', message='Passwords must match.')])
+    password2 = PasswordField('Confirm password', validators=[DataRequired()])
+
+    def __get_pwned(self, password):
+        sha1 = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+        prefix, suffix = sha1[:5], sha1[5:]
+        response = requests.get(f'https://api.pwnedpasswords.com/range/{prefix}')
+        hashes = (line.split(':') for line in response.text.splitlines())
+        for h, count in hashes:
+            if h == suffix:
+                return int(count)
+
+    def validate_password(self, field):
+        count = self.__get_pwned(field.data)
+        threshold = 100
+        if count and count > threshold:
+            raise ValidationError(f'This password has appeared in {count} data breaches. Please choose a more secure ont.')
+
+    submit = SubmitField('Reset Password')
+
+class PasswordResetRequestForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
+    submit = SubmitField('Reset Password')
+
+    def validate_email(self, field):
+        if not User.query.filter_by(email=field.data).first():
+            raise ValidationError('No account found with this email.')
