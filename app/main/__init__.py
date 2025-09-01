98f992f2f28e8033923ac3e6164b2e20e83b3f0c
byronlove-lace <byronlovelace@proton.me>
2025-09-01 14:30:03 +0100

    Added context processor for Permissions consts.

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,5 +1,10 @@
 from flask import Blueprint
 
-main = Blueprint('main', __name__) 
+main = Blueprint('main', __name__)
 
 from . import views, errors
+from ..models import Permission
+
+@main.app_context_processor #auto-injects keywords into every render_template call -- Permission consts
+def inject_permissions():
+    return dict(Permission=Permission)
