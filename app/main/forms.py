5a65720514d1b0b8845a3dc5c4fef6cc13af7ba2
byronlove-lace <byronlovelace@proton.me>
2025-09-03 16:53:33 +0100

    Added Admin Edit Profile form.

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,21 +1,42 @@
 from flask_wtf import FlaskForm
-from wtforms import BooleanField, PasswordField, StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms import BooleanField, PasswordField, SelectField, StringField, SubmitField, TextAreaField, ValidationError
+from wtforms.validators import DataRequired, Length, Email, Regexp
+from ..models import User, Role
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     secret = PasswordField('Tell me your secret.', validators=[DataRequired(), Length(6)])
     submit = SubmitField('Submit')
 
-class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Length(1, 64),
-                                             Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Keep me logged in')
-    submit = SubmitField('Log In')
-
 class EditProfileForm(FlaskForm):
     name = StringField('Real name', validators=[Length(0, 64)])
     location = StringField('Location', validators=[Length(0, 64)])
     about_me = TextAreaField('About me')
     submit = SubmitField('Submit')
+
+class EditProfileAdminForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
+    username = StringField('Username', validators=[DataRequired(), Length(1, 64),
+                                                   Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                                                          'Usernames must have only letters,'
+                                                          'numbers, dots or underscores')])
+    confirmed = BooleanField('Confirmed')
+    role = SelectField('Role', coerce=int) # list of (val, label) tuples
+    name = StringField('Real name', validators=[Length(0, 64)])
+    location = StringField('Location', validators=[Length(0, 64)])
+    about_me = TextAreaField('About me')
+    submit = SubmitField('Submit')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered.')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username already in use.')
+
+    def __init__(self, user, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.role.choices = [(role.id, role.name)
+                              for role in Role.query.order_by(Role.name).all()]
+        self.user = user
