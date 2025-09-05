1fbc3248eb33dbbb06a85db2105b145a31ce7606
byronlove-lace <byronlovelace@proton.me>
2025-09-05 20:21:06 +0100

    Added Edit Post functionality.

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,7 @@
-from datetime import datetime, timezone
-from flask import render_template, session, redirect, url_for, flash, request, current_app
+from flask import render_template, redirect, url_for, flash, request, current_app, abort
 from flask_login import login_required, current_user
 from . import main
-from .forms import NameForm, EditProfileForm, EditProfileAdminForm, PostForm
+from .forms import EditProfileForm, EditProfileAdminForm, PostForm
 from .. import db
 from ..models import Permission, User, Post
 from ..decorators import admin_required
@@ -83,3 +82,21 @@ def edit_profile_admin(id):
 def post(id):
     post = Post.query.get_or_404(id)
     return render_template('post.html', posts=[post])
+
+@main.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    post = Post.query.get_or_404(id)
+    if current_user != post.author and \
+        not current_user.is_administrator():
+            abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.body = form.body.data
+        db.session.add(post)
+        db.session.commit()
+        flash('The post has been updated.')
+        return redirect(url_for('.post', id=post.id))
+    form.body.data = post.body
+    return render_template('edit_post.html', form=form)
+
+
