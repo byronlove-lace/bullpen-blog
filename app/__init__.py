2513ed0ad90f864a0a3d61c8ae463166f2722a78
byronlove-lace <byronlovelace@proton.me>
2025-09-12 16:36:19 +0100

    Registered API blueprint.

--- app/__init__.py
+++ app/__init__.py
@@ -51,6 +51,9 @@ def create_app(config_name):
     from.auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
+    from .api import api as api_blueprint
+    app.register_blueprint(api_blueprint, url_prefix='/api/v1')
+
     login_manager.init_app(app)
 
     # attach routes or custom error pages here
