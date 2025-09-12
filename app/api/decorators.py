d692f5242a72c15fa50067e742fc5ca7a990bade
byronlove-lace <byronlovelace@proton.me>
2025-09-12 16:34:59 +0100

    Added premission decorators for API requests.

---
+++ app/api/decorators.py
@@ -0,0 +1,12 @@
+from functools import wraps
+from flask import abort, g
+
+def permission_required(permission):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if not g.current_user.can(permission):
+                abort(403)
+            return f(*args, **kwargs)
+        return decorated_function
+    return decorator
