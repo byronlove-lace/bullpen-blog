bfaeeb5c602ae1ca8f93dc84ad35b9bbde090295
byronlove-lace <byronlovelace@proton.me>
2025-10-06 15:24:50 +0200

    Deploy now chacks for params.json

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
+
 set -euo pipefail
 
 az deployment sub create \
   --name bullpen-deployment \
   --location northeurope \
-  --template-file infra/rg.bicep
+  --template-file infra/rg.bicep \
+  --parameters-json @infra/rg.bicep
