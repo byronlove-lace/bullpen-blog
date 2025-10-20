b9d45240766c24f84d76ed41560c9a555d6a6cdc
byronlove-lace <byronlovelace@proton.me>
2025-10-20 13:08:41 +0200

    Stripped keyvault fetching from config. Separated infra and app. Secrets dynamically loaded into docker at run. 12 Factor App compliant.

--- config.py
+++ config.py
@@ -1,32 +1,10 @@
 import os
-import json
-from azure.identity import DefaultAzureCredential
-from azure.keyvault.secrets import SecretClient
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-# Source KV name from params.json for single source of truth
-params_path = os.path.join(basedir, "infra", "params.json")
-with open(params_path, "r") as f:
-    params = json.load(f)
-
-AZURE_KEY_VAULT_NAME = params.get("keyVaultName", {}).get("value")
-if not AZURE_KEY_VAULT_NAME:
-    raise RuntimeError("Key Vault name not found in infra/params.json")
-
-def get_secret(secret_name):
-    key_vault_uri = f"https://{AZURE_KEY_VAULT_NAME}.vault.azure.net/"
-
-    try:
-        credential = DefaultAzureCredential()
-        client = SecretClient(vault_url=key_vault_uri, credential=credential)
-        return client.get_secret(secret_name).value
-    except Exception as e:
-        raise RuntimeError(f"Failed to retrieve {secret_name} from Key Vault: {e}")
-
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv("SECRET_KEY")
+    SECRET_KEY = os.getenv("DEV_SECRET_KEY")
 
     # Dev/Testing Mail Stuff
     try:
@@ -37,16 +15,16 @@ class Config:
     MAIL_USERNAME = os.getenv("DEV_MAIL_USERNAME")
     MAIL_PASSWORD = os.getenv("DEV_MAIL_PASSWORD")
     MAIL_USE_TLS = os.getenv("MAIL_USE_TLS", "true").lower() == "true"
-    MAIL_USE_SSL = os.getenv("MAIL_USE_SSL", "false").lower() == "false"
+    MAIL_USE_SSL = os.getenv("MAIL_USE_SSL", "false").lower() == "true"
 
-    BULLPEN_MAIL_SUBJECT_PREFIX = '[Bullpen]'
-    BULLPEN_MAIL_SENDER = f"Bullpen Admin <{MAIL_USERNAME}>"
-    BULLPEN_ADMIN_MAIL = os.getenv("BULLPEN_ADMIN_MAIL")
+    MAIL_SUBJECT_PREFIX = '[Bullpen]'
+    MAIL_SENDER = f"Bullpen Admin <{MAIL_USERNAME}>"
+    ADMIN_MAIL = os.getenv("ADMIN_MAIL")
 
     # Pages
-    BULLPEN_POSTS_PER_PAGE = int(os.getenv("BULLPEN_POSTS_PER_PAGE", 20))
-    BULLPEN_COMMENTS_PER_PAGE = int(os.getenv("BULLPEN_COMMENTS_PER_PAGE", 20))
-    BULLPEN_FOLLOWERS_PER_PAGE = int(os.getenv("BULLPEN_FOLLOWERS_PER_PAGE", 20))
+    POSTS_PER_PAGE = int(os.getenv("POSTS_PER_PAGE", 20))
+    COMMENTS_PER_PAGE = int(os.getenv("COMMENTS_PER_PAGE", 20))
+    FOLLOWERS_PER_PAGE = int(os.getenv("FOLLOWERS_PER_PAGE", 20))
 
     # Cookies
     SHOW_FOLLOWED_COOKIE_AGE=int(os.getenv('SHOW_FOLLOWED_COOKIE_AGE', 2592000))
@@ -64,39 +42,58 @@ class Config:
     LOOPBACK_ADDRESS=os.getenv('LOOPBACK_ADDRESS')
 
     # Performance
-    BULLPEN_SLOW_DB_QUERY_TIME=float(os.environ.get('BULLPEN_SLOW_DB_QUERY_TIME', 0.5))
-    SQLALCHEMY_RECORD_QUERIES=os.environ.get('SQLALCHEMY_RECORD_QUERIES', "true").lower() == "true"
+    SLOW_DB_QUERY_TIME=float(os.getenv('SLOW_DB_QUERY_TIME', 0.5))
+    SQLALCHEMY_RECORD_QUERIES=os.getenv('SQLALCHEMY_RECORD_QUERIES', "true").lower() == "true"
 
-    @staticmethod
-    def init_app(app):
+    @classmethod
+    def init_app(cls, app):
         pass
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DEV_DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DEV_DATABASE_URI")
 
 class TestingConfig(Config):
     TESTING = True
     WTF_CSRF_ENABLED = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get("TEST_DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = os.getenv("TEST_DATABASE_URI")
 
 class ProductionConfig(Config):
     PRODUCTION = True
 
-    SECRET_KEY = get_secret("SecretKey")
+    SECRET_KEY = os.getenv('PROD_SECRET_KEY')
 
-    SQLALCHEMY_DATABASE_URI = get_secret("ProdDatabaseURI")
-    SERVER_NAME = os.environ.get("PROD_SERVER_NAME")
+    SQLALCHEMY_DATABASE_URI = os.getenv('PROD_DB_URI')
+    SERVER_NAME = os.getenv("PROD_WEBAPP_HOSTNAME")
 
     # PROD MAIL
     MAIL_SERVER = os.getenv("PROD_MAIL_SERVER")
-    MAIL_USERNAME = os.getenv("PROD_SANDBOX_MAIL_USERNAME")
-    MAIL_PASSWORD= get_secret("ProdSandboxMailPassword")
+    MAIL_USERNAME = os.getenv("PROD_MAIL_USERNAME")
+    MAIL_PASSWORD= os.getenv('PROD_MAIL_PASSWORD')
+
+    @classmethod
+    def init_app(cls, app):
+        from werkzeug.middleware.proxy_fix import ProxyFix
+
+        app.wsgi_app = ProxyFix(app.wsgi_app)
+
+class DockerConfig(ProductionConfig):
+    @classmethod
+    def init_app(cls, app):
+        ProductionConfig.init_app(app)
+
+        # log to stderr
+        import logging
+        from logging import StreamHandler
+        file_handler = StreamHandler()
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
 
 config = {
     'development': DevelopmentConfig,
     'testing': TestingConfig,
     'production': ProductionConfig,
+    'docker': DockerConfig,
 
     'default': DevelopmentConfig
 }
